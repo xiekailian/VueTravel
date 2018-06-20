6306cdfe111d181150a93be93a0bc1301999b506
xiekailian <460715662@qq.com>
2018-06-20 21:06:01 +0800

    加入border和reset适应性配置，适应各种移动设备

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 
