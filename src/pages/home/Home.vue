75b1d4d8e68a33c1df8fb8f7e73f07dc4116dbe0
xiekailian <460715662@qq.com>
2018-06-20 20:59:33 +0800

    Initial commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style>
+
+</style>
