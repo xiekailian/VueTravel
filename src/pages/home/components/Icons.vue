425728c0568f07e350371292e1044998a9dee9a5
xiekailian <460715662@qq.com>
2018-06-23 21:27:12 +0800

    完成菜单栏目页面设计

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl"/>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+
+      <!--<swiper-slide>-->
+      <!--<div class="icon">-->
+      <!--<div class="icon-img">-->
+      <!--<img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>-->
+      <!--</div>-->
+      <!--<p class="icon-desc">景点门票</p>-->
+      <!--</div>-->
+      <!--</swiper-slide>-->
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        iconList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '必游榜单'
+        }, {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc: '水上玩乐'
+        }, {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        }, {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '名胜古迹'
+        }, {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/c1/6f15f887179fa002.png',
+          desc: '夫子庙'
+        }, {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fa/2548667cb6e902.png',
+          desc: '总统府'
+        }, {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '中山陵'
+        }, {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc: '水上玩乐'
+        }]
+      }
+    },
+    computed: {
+      pages () {
+        const pages = [];
+        this.iconList.forEach((item,index)=>{
+          const page = Math.floor(index/8);
+          if (!pages[page]){
+            pages[page] = [];
+          }
+          pages[page].push(item);
+        });
+        return pages;
+      }
+    }
+
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .
+  icons > > >
+  .swiper-container
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        //以下设计文字溢出时的样式
+        ellipse()
+
+</style>
+
