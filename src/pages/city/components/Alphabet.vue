7888c0ee377bda727c0611af5010fb635e77e78d
xiekailian <460715662@qq.com>
2018-07-01 21:14:52 +0800

    完成字母表和右侧cityList的联动

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,13 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+    <li class="item" v-for="item of letters" :key="item" :ref="item"
+        @click="handleLetterClick"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+    >
+      {{item}}
+    </li>
   </ul>
 
 </template>
@@ -10,6 +17,41 @@
     name: 'CityAlphabet',
     props: {
       cities: Object
+    },
+    data () {
+      return {
+        touchStatus: false
+      }
+    },
+    computed: {
+      letters () {
+        const letters = []
+        for (let i in this.cities) {
+          letters.push(i)
+        }
+        return letters
+      }
+    },
+    methods: {
+      handleLetterClick (e) {
+        this.$emit('change', e.target.innerText)
+      },
+      handleTouchStart (e) {
+        this.touchStatus = true
+      },
+      handleTouchMove (e) {
+        if (this.touchStatus) {
+          const startY = this.$refs['A'][0].offsetTop
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change',this.letters[index])
+          }
+        }
+      },
+      handleTouchEnd (e) {
+        this.touchStuts = false
+      }
     }
   }
 </script>
